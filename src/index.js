d492dd7c106e15289bf795dbd08b2e65f5d85816
KirBoy <kirll.boy@yandex.ru>
2021-12-12 17:14:11 +0300

    Сделал 7 ДЗ

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from "./tutor-6/App";
+import App from "./tutor-7/App";
 
 ReactDOM.render(
     <React.StrictMode>
