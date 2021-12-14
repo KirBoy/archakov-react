f6ff61b56005bf40a0e92ab05585c698b1550c19
KirBoy <kirll.boy@yandex.ru>
2021-12-14 14:21:38 +0300

    Сделал 8 ДЗ

---
+++ src/tutor-8/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./tutor-7.css";
+
+
+function App() {
+    const [users, setUsers] = React.useState([])
+
+    async function getUsers(name) {
+        try {
+            let response = await fetch('https://61b87c6b64e4a10017d18fe5.mockapi.io/users')
+            let users = await response.json();
+            setUsers(users)
+        } catch (e) {
+            alert('нету пользователей')
+        }
+    }
+
+    return (
+        <>
+            {users.map(el => <h1 key={el.id}>{el.name}</h1>)}
+            <button onClick={getUsers}>Запросить пользователей</button>
+
+        </>
+
+    );
+}
+
+export default App;
